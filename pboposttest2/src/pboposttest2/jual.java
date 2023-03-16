498810f8ad145544d7c6d2ffb269a00ee24956b5
Marlina Yunus <106863346+Merlynna@users.noreply.github.com>
2023-03-16 20:27:09 +0800

    Tugas Posttest 2 PBO

---
+++ pboposttest2/src/pboposttest2/jual.java
@@ -0,0 +1,34 @@
+
+package pboposttest2;
+
+public class jual {
+    private String nama;
+    private String pembeli;
+    private int harga;
+    
+    //setter
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setPembeli(String pembeli) {
+        this.pembeli = pembeli;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    //getter
+    public String getNama() {
+        return nama;
+    }
+
+    public String getPembeli() {
+        return pembeli;
+    }
+
+    public int getHarga() {
+        return harga;
+    }   
+}
---
+++ pboposttest2/test/pboposttest2/Pboposttest2Test.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboposttest2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author hp
+ */
+public class Pboposttest2Test {
+    
+    public Pboposttest2Test() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Pboposttest2.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Pboposttest2.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of clearScreen method, of class Pboposttest2.
+     */
+    @Test
+    public void testClearScreen() {
+        System.out.println("clearScreen");
+        Pboposttest2.clearScreen();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of showMenu method, of class Pboposttest2.
+     */
+    @Test
+    public void testShowMenu() {
+        System.out.println("showMenu");
+        Pboposttest2.showMenu();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of backToMenu method, of class Pboposttest2.
+     */
+    @Test
+    public void testBackToMenu() {
+        System.out.println("backToMenu");
+        Pboposttest2.backToMenu();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of readTodoList method, of class Pboposttest2.
+     */
+    @Test
+    public void testReadTodoList() {
+        System.out.println("readTodoList");
+        Pboposttest2.readTodoList();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of showTodoList method, of class Pboposttest2.
+     */
+    @Test
+    public void testShowTodoList() {
+        System.out.println("showTodoList");
+        Pboposttest2.showTodoList();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addTodoList method, of class Pboposttest2.
+     */
+    @Test
+    public void testAddTodoList() {
+        System.out.println("addTodoList");
+        Pboposttest2.addTodoList();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of editTodoList method, of class Pboposttest2.
+     */
+    @Test
+    public void testEditTodoList() {
+        System.out.println("editTodoList");
+        Pboposttest2.editTodoList();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of deleteTodoList method, of class Pboposttest2.
+     */
+    @Test
+    public void testDeleteTodoList() {
+        System.out.println("deleteTodoList");
+        Pboposttest2.deleteTodoList();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
